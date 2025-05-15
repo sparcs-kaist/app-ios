6fc9a09159f216d2471a87ade60aaec7590fc842
Soongyu Kwon <me@soongyu.me>
2025-05-15 23:55:11 +0900

    Create PostView.swift

---
+++ soap/Features/Post/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  soap
+//
+//  Created by Soongyu Kwon on 15/05/2025.
+//
+
+import SwiftUI
+
+struct PostView: View {
+  var body: some View {
+    Text("PostView")
+  }
+}
+
+#Preview {
+  NavigationStack {
+    PostView()
+  }
+}
