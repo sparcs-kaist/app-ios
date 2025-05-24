0caa5c94139bcac18c84c467e68602b7107f7154
Soongyu Kwon <me@soongyu.me>
2025-05-24 18:22:29 +0900

    post header

--- soap/Features/Post/PostView.swift
+++ soap/Features/Post/PostView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct PostView: View {
   var body: some View {
-    Text("PostView")
+    VStack(alignment: .leading, spacing: 8) {
+      Text("[대학원 동아리 연합회] 대학원 동아리 연합회 소속이 되실 동아리를 모집합니다! (feat. 2022년 하반기 동아리 등록 심사 위원회)")
+        .font(.headline)
+
+      HStack {
+        Text("22 May 2025 16:22")
+        Text("485 views")
+      }
+      .font(.caption)
+      .foregroundStyle(.secondary)
+
+      HStack {
+        Circle()
+          .frame(width: 28, height: 28)
+
+        Text("류형욱(전산학부)")
+
+        Image(systemName: "chevron.right")
+      }
+      .font(.subheadline)
+
+      Divider()
+        .padding(.vertical, 8)
+    }
+    .padding()
   }
 }
 
