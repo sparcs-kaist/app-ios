345c8fc5a68e94bd693c6001aa3aad82ae4c3ec1
Soongyu Kwon <me@soongyu.me>
2025-06-13 16:26:36 +0900

    put timetableview inside the navigationstack

--- soap/Features/Timetable/TimetableView.swift
+++ soap/Features/Timetable/TimetableView.swift
@@ -14,51 +14,52 @@ struct TimetableView: View {
   @FocusState private var isFocused: Bool
 
   var body: some View {
-    GeometryReader { geometry in
-      ScrollView {
-        VStack(spacing: 0) {
-          // Timetable Selector
-          CompactTimetableSelector()
-            .padding(.horizontal)
-            .padding(.horizontal, 4)
-          //                            .padding(.leading)
-          // Timetable Gird View
-          ZStack {
-            RoundedRectangle(cornerRadius: 16)
-              .fill(.white)
-            TimetableGrid()
-              .padding(8)
-          }
-          .frame(height: geometry.size.height * 0.58)
-          .padding([.horizontal, .bottom])
-          .padding(.top, 8)
+    NavigationStack {
+      GeometryReader { geometry in
+        ScrollView {
+          VStack(spacing: 0) {
+            // Timetable Selector
+            CompactTimetableSelector()
+              .padding(.horizontal)
+              .padding(.horizontal, 4)
+            //                            .padding(.leading)
+            // Timetable Gird View
+            ZStack {
+              RoundedRectangle(cornerRadius: 16)
+                .fill(.white)
+              TimetableGrid()
+                .padding(8)
+            }
+            .frame(height: geometry.size.height * 0.58)
+            .padding([.horizontal, .bottom])
+            .padding(.top, 8)
 
-          // Timetable Summary View
-          ZStack {
-            RoundedRectangle(cornerRadius: 16)
-              .fill(.white)
-            TimetableSummary()
-              .padding(8)
-              .padding(.vertical, 8)
+            // Timetable Summary View
+            ZStack {
+              RoundedRectangle(cornerRadius: 16)
+                .fill(.white)
+              TimetableSummary()
+                .padding(8)
+                .padding(.vertical, 8)
+            }
+            .padding(.horizontal)
           }
-          .padding(.horizontal)
         }
+        .navigationTitle("Timetable")
+        .background(Color(UIColor.secondarySystemBackground))
+        .ignoresSafeArea(.keyboard)
+      }
+      .task {
+        // fetch data
+        await viewModel.fetchData()
       }
       .navigationTitle("Timetable")
-      .background(Color(UIColor.secondarySystemBackground))
-      .ignoresSafeArea(.keyboard)
-    }
-    .task {
-      // fetch data
-      await viewModel.fetchData()
     }
   }
 }
 
 #Preview {
-  NavigationStack {
-    TimetableView()
-      .environment(TimetableViewModel())
-  }
+  TimetableView()
+    .environment(TimetableViewModel())
 }
 
