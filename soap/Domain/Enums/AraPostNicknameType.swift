a89dab54433128bc14be4c6cfe05b600e4a6d9fb
Soongyu Kwon <me@soongyu.me>
2025-08-10 06:44:27 +0900

    models

---
+++ soap/Domain/Enums/AraPostNicknameType.swift
@@ -0,0 +1,13 @@
+//
+//  AraPostNicknameType.swift
+//  soap
+//
+//  Created by Soongyu Kwon on 10/08/2025.
+//
+
+import Foundation
+
+enum AraPostNicknameType: String {
+  case anonymous = "ANONYMOUS"
+  case regular = "REGULAR"
+}
