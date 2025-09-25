43407d16960cc5bb00895604cc2415952d1e6daf
Soongyu Kwon <me@soongyu.me>
2025-09-25 16:10:35 +0900

    initialise firebase

--- soap/soapApp.swift
+++ soap/soapApp.swift
@@ -7,10 +7,23 @@
 
 import SwiftUI
 import SwiftyBeaver
+import FirebaseCore
+
 let logger = SwiftyBeaver.self
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct soapApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
   init() {
     // Initialise Console Logger (SwiftyBeaver)
     let console = ConsoleDestination()
