ec81a45de9311f49d0326561abe5e51fef5b2e0f
Jungwoo Ha <cubem10@kakao.com>
2025-10-02 02:46:31 +0900

    add proper error handling to SettingsView
    Signed-off-by: Jungwoo Ha <cubem10@kakao.com>

--- soap/Features/Settings/SettingsView.swift
+++ soap/Features/Settings/SettingsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingsView: View {
   @Environment(\.openURL) private var openURL
   @State private var viewModel: SettingsViewModel
+  @State private var showLogoutError: Bool = false
   
   init(_ viewModel: SettingsViewModel = .init()) {
     self.viewModel = viewModel
@@ -37,13 +38,20 @@ struct SettingsView: View {
         }
       }
       .navigationTitle(Text("Settings"))
+      .alert("Error", isPresented: $showLogoutError) {
+        Button(role: .confirm) { }
+      } message: {
+        Text("An error occurred while signing out. Please try again later.")
+      }
     }
   }
   
   private var appSettings: some View {
     Group {
       Button("Change Language", systemImage: "globe") {
-        UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+        if let url = URL(string: UIApplication.openSettingsURLString) {
+          openURL(url)
+        }
       }
       Button("Send Feedback", systemImage: "exclamationmark.bubble") {
         if let url = URL(string: "mailto:app@sparcs.org"), UIApplication.shared.canOpenURL(url) {
