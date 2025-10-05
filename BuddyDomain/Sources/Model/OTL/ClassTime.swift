74d3606c82a0ab20fc78520f7fed7dce42d6f4ff
Soongyu Kwon <me@soongyu.me>
2025-10-06 04:12:45 +0900

    ClassTime description and statusString

--- BuddyDomain/Sources/Model/OTL/ClassTime.swift
+++ BuddyDomain/Sources/Model/OTL/ClassTime.swift
@@ -20,6 +20,45 @@ public struct ClassTime: Sendable {
     end - begin
   }
 
+  public var description: String {
+    func formatTime(_ minutes: Int) -> String {
+      let hours = minutes / 60
+      let mins = minutes % 60
+      return String(format: "%02d:%02d", hours, mins)
+    }
+    return "\(formatTime(begin))-\(formatTime(end))"
+  }
+
+  public var statusString: String {
+    let calendar = Calendar.current
+    let now = Date()
+    let components = calendar.dateComponents([.hour, .minute], from: now)
+    let currentMinutes = (components.hour ?? 0) * 60 + (components.minute ?? 0)
+
+    if currentMinutes < begin {
+      // Class hasn't started yet
+      let diff = begin - currentMinutes
+      return "in \(formatMinutes(diff))"
+    } else if currentMinutes >= end {
+      // Class already ended
+      let diff = currentMinutes - end
+      return "ended \(formatMinutes(diff)) ago"
+    } else {
+      // Class is ongoing
+      return "now"
+    }
+  }
+
+  private func formatMinutes(_ minutes: Int) -> String {
+    let hours = minutes / 60
+    let mins = minutes % 60
+    if hours > 0 {
+      return "\(hours)h \(mins)m"
+    } else {
+      return "\(mins)m"
+    }
+  }
+
   public init(
     buildingCode: String,
     classroomName: LocalizedString,
