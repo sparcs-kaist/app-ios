682f09e73b4c4dda2fd4925ca5747ad5421f3d14
Jungwoo Ha <cubem10@kakao.com>
2025-10-07 19:46:38 +0900

    report exceptions from TaxiReportView
    Signed-off-by: Jungwoo Ha <cubem10@kakao.com>

--- soap/Features/TaxiReport/TaxiReportViewModel.swift
+++ soap/Features/TaxiReport/TaxiReportViewModel.swift
@@ -21,6 +21,7 @@ class TaxiReportViewModel {
   
   // MARK: - Dependency
   @ObservationIgnored @Injected(\.taxiReportRepository) private var taxiReportRepository: TaxiReportRepositoryProtocol
+  @ObservationIgnored @Injected(\.crashlyticsHelper) private var crashlyticsHelper: CrashlyticsHelper
   
   // MARK: - Functions
   public func createReport(roomID: String) async throws {
@@ -38,5 +39,10 @@ class TaxiReportViewModel {
     )
     
     let _ = try await taxiReportRepository.createReport(with: requestModel)
+  }
+  
+  public func handleException(_ error: Error) {
+    logger.error("[TaxiReportViewModel] failed to create a report: \(error)")
+    crashlyticsHelper.recordException(error: error)
   }
 }
