2640bf5211eb67967edcdc95c94d148e631bcf73
Soongyu Kwon <me@soongyu.me>
2025-10-07 20:10:19 +0900

    UpcomingClassCornerWidgetView

--- BuddyWatchWidget/BuddyWatchWidget.swift
+++ BuddyWatchWidget/BuddyWatchWidget.swift
@@ -255,25 +255,7 @@ struct UpcomingClassCircularWidgetView: View {
   }
 }
 
-struct UpcomingClassCornerWidgetView: View {
-  var entry: LectureEntry
 
-  var body: some View {
-    if let lecture = entry.lecture, let start = entry.startDate {
-      Text(start, style: .time)
-        .widgetCurvesContent()
-        .widgetLabel(lecture.title.localized())
-    } else if entry.signInRequired {
-      Text("")
-        .widgetCurvesContent()
-        .widgetLabel("Sign in Required")
-    } else {
-      Text("")
-        .widgetCurvesContent()
-        .widgetLabel("No more classes")
-    }
-  }
-}
 
 struct BuddyWatchWidgetEntryView: View {
   @Environment(\.widgetFamily) private var familiy
