48fd7989b4c1768ace3ee7efd98ced7a14aea0d0
Soongyu Kwon <me@soongyu.me>
2025-10-07 20:10:36 +0900

    UpcomingClassCircularWidgetView

--- BuddyWatchWidget/BuddyWatchWidget.swift
+++ BuddyWatchWidget/BuddyWatchWidget.swift
@@ -232,28 +232,7 @@ struct UpcomingClassInlineWidgetView: View {
   }
 }
 
-struct UpcomingClassCircularWidgetView: View {
-  var entry: LectureEntry
 
-  var body: some View {
-    if let start = entry.startDate {
-      VStack {
-        Image(systemName: "calendar")
-        Text(start, style: .time)
-      }
-    } else if entry.signInRequired {
-      VStack {
-        Image(systemName: "arrow.up.right.square")
-        Text("Sign in")
-      }
-    } else {
-      VStack {
-        Image(systemName: "graduationcap")
-        Text("0 left")
-      }
-    }
-  }
-}
 
 
 
