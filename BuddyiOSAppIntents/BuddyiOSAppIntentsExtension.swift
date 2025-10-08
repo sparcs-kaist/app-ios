249a58ce9f7c68a4fbaec697f17e8e2845621558
Soongyu Kwon <me@soongyu.me>
2025-10-09 04:30:48 +0900

    add support app intents

---
+++ BuddyiOSAppIntents/BuddyiOSAppIntentsExtension.swift
@@ -0,0 +1,14 @@
+//
+//  BuddyiOSAppIntentsExtension.swift
+//  BuddyiOSAppIntents
+//
+//  Created by Soongyu Kwon on 09/10/2025.
+//
+
+import AppIntents
+import ExtensionFoundation
+
+@main
+struct BuddyiOSAppIntentsExtension: AppIntentsExtension {
+  
+}
