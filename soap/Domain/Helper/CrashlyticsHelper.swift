beb2be426bcfe7c7962532d160784315a9a20735
Jungwoo Ha <cubem10@kakao.com>
2025-10-29 23:49:17 +0900

    do not report exception occurred by network loss
    Signed-off-by: Jungwoo Ha <cubem10@kakao.com>

--- soap/Domain/Helper/CrashlyticsHelper.swift
+++ soap/Domain/Helper/CrashlyticsHelper.swift
@@ -8,7 +8,7 @@
 import Foundation
 import FirebaseCrashlytics
 import Moya
-import SwiftyBeaver
+import Alamofire
 
 @MainActor
 @Observable
@@ -17,6 +17,16 @@ final class CrashlyticsHelper {
   var alertMessage: LocalizedStringResource = ""
   
   func recordException(error: Error, showAlert: Bool = true, alertMessage: LocalizedStringResource = "Something went wrong. Please try again later.") {
+    if let error = error as? MoyaError,
+       case .underlying(let afError, _) = error,
+       let underlyingError = (afError as? Alamofire.AFError)?.underlyingError,
+       [NSURLErrorNotConnectedToInternet, NSURLErrorDataNotAllowed].contains((underlyingError as NSError).code)
+    {
+      self.showAlert = showAlert
+      self.alertMessage = "You are not connected to the Internet."
+      return
+    }
+    
     Crashlytics.crashlytics().record(error: error as NSError)
     self.showAlert = showAlert
     self.alertMessage = alertMessage
