d3e4f1d746a1c470966d063f6b7529512e8ca4ff
Soongyu Kwon <me@soongyu.me>
2025-12-25 16:30:53 +0900

    iOS widget target

---
+++ BuddyiOSWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  BuddyiOSWidget
+//
+//  Created by Soongyu Kwon on 25/12/2025.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
---
+++ BuddyiOSWidget/BuddyiOSWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  BuddyiOSWidgetBundle.swift
+//  BuddyiOSWidget
+//
+//  Created by Soongyu Kwon on 25/12/2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BuddyiOSWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BuddyiOSWidget()
+//        BuddyiOSWidgetControl()
+//        BuddyiOSWidgetLiveActivity()
+    }
+}
