6740a2966a48999f26970aa514e31fc4d86475cc
Soongyu Kwon <me@soongyu.me>
2026-09-24 02:45:56 +0900

    timetable sharing render view

---
+++ BuddyFeature/Sources/BuddyFeatureTimetable/TimetableShare/TimetableShareRenderingView.swift
@@ -0,0 +1,101 @@
+//
+//  TimetableShareRenderingView.swift
+//  BuddyFeature
+//
+//  Created by Soongyu Kwon on 9/23/26.
+//
+
+import SwiftUI
+import BuddyDomain
+import TimetableUI
+
+struct TimetableShareRenderingView: View {
+	let semester: Semester
+	let timetable: Timetable
+	
+	@Environment(\.timetableTheme) private var theme
+	
+	var body: some View {
+		ZStack {
+			Color.clear
+			
+			VStack {
+				ThemedGridCard {
+					TimetableGrid(
+						selectedTimetable: timetable,
+						placement: .render
+					)
+				}
+				.padding([.top, .horizontal])
+				// Layer 1: Tight contact shadow
+				.shadow(
+					color: Color.black.opacity(0.04),
+					radius: 2,
+					x: 0,
+					y: 1
+				)
+				// Layer 2: Soft ambient spread
+				.shadow(
+					color: Color.black.opacity(0.08),
+					radius: 16,
+					x: 0,
+					y: 8
+				)
+				
+				TimetableShareRenderingViewHeader(semester: semester.description, credits: timetable.credits)
+			}
+			.background(.white, in: .rect(cornerRadius: 36))
+			.padding()
+		}
+		.frame(width: 390, height: 844)
+		.preferredColorScheme(.light)
+	}
+}
+
+private struct TimetableShareRenderingViewHeader: View {
+	let semester: String
+	let credits: Int
+	
+	var body: some View {
+		HStack {
+			Image(.buddyIcon)
+				.resizable()
+				.scaledToFit()
+				.frame(width: 32, height: 32)
+				.foregroundStyle(.tertiary)
+			
+			VStack(alignment: .leading) {
+				Text("Timetable")
+					.textCase(.uppercase)
+					.font(.caption.weight(.medium))
+					.foregroundStyle(.secondary)
+				
+				Text(semester)
+					.font(.headline)
+			}
+			
+			Spacer()
+			
+			VStack(alignment: .trailing) {
+				Text("Credits")
+					.foregroundStyle(.tertiary)
+					.font(.caption2)
+					.fontWeight(.medium)
+					.textCase(.uppercase)
+				
+				Text("\(credits)")
+					.foregroundStyle(.secondary)
+					.fontDesign(.rounded)
+					.fontWeight(.semibold)
+			}
+		}
+		.padding(.top, 4)
+		.padding([.horizontal, .bottom])
+		.padding(.horizontal, 8)
+	}
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+	TimetableShareRenderingView(semester: Semester.mock, timetable: Timetable.mock)
+		.environment(\.timetableTheme, .builtIn[3])
+}
