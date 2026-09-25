ed8844391e126f37b346216dcef2d76aa86bb1ab
Soongyu Kwon <me@soongyu.me>
2026-09-25 18:10:30 +0900

    Create CreditCalculationView.swift

---
+++ BuddyFeature/Sources/BuddyFeatureTimetable/CreditCalculation/CreditCalculationView.swift
@@ -0,0 +1,26 @@
+//
+//  CreditCalculationView.swift
+//  BuddyFeature
+//
+//  Created by Soongyu Kwon on 9/25/26.
+//
+
+import SwiftUI
+import BuddyDomain
+import TimetableUI
+
+struct CreditCalculationView: View {
+	var body: some View {
+		VStack {
+			
+		}
+		.navigationTitle("Credits")
+	}
+}
+
+
+#Preview {
+	NavigationStack {
+		CreditCalculationView()
+	}
+}
